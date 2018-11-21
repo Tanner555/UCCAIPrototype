7f0da6805ab04eb6bffb7f48edd505ff493d4036
Tanner555 <theman989898@gmail.com>
2018-11-21 08:12:16 -0800

    Fixed Zooming Bug, Added New View Type

---
+++ Assets/Tactical Prototyping/Scripts/TPC Overrides/RTSThirdPersonViewType.cs
@@ -0,0 +1,28 @@
+﻿using Opsive.UltimateCharacterController.Events;
+using Opsive.UltimateCharacterController.Game;
+using Opsive.UltimateCharacterController.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTSPrototype
+{
+    public class RTSThirdPersonViewType : Opsive.UltimateCharacterController.ThirdPersonController.Camera.ViewTypes.Combat
+    {
+        bool bHasBeenActivated = false;
+
+        public override void ChangeViewType(bool activate, float pitch, float yaw, Quaternion characterRotation)
+        {
+            if (activate && bHasBeenActivated == false)
+            {
+                base.ChangeViewType(activate, pitch, yaw, characterRotation);
+            }
+            else if(activate == false)
+            {
+                base.ChangeViewType(activate, pitch, yaw, characterRotation);
+            }
+
+            bHasBeenActivated = true;
+        }
+    }
+}
