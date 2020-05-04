f65c733d01ba15999aa7149347ecdb808e0c4c55
Tanner555 <theman989898@gmail.com>
2020-05-04 01:15:31 -0700

    Updated Packages, Removed UIERuntime Package

---
+++ Assets/Tactical Prototyping/Scripts/UI/MainMenuUEIRuntimePrototype.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+//using Unity.UIElements.Runtime;
+using BaseFramework;
+
+namespace RTSPrototype
+{
+    public class MainMenuUEIRuntimePrototype : MainMenuHandler
+    {
+        //public PanelRenderer m_MainMenuScreen;
+
+        //private void OnEnable()
+        //{
+        //    if(m_MainMenuScreen != null)
+        //    {
+        //        m_MainMenuScreen.postUxmlReload += BindMainMenuScreen;
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("No Main Menu Panel Assigned To Handler.");
+        //    }
+        //}
+
+        //private void Start()
+        //{
+        //    if (m_MainMenuScreen != null)
+        //    {
+        //        RTSUIEHelper.SetScreenEnableState(ref m_MainMenuScreen, true);
+        //    }
+        //}
+
+        //private void OnDisable()
+        //{
+        //    DisableMainMenuScreenIfEnabled();
+        //}
+
+        //public override void Btn_PlayGame()
+        //{
+        //    Debug.Log("Play game called");
+        //    DisableMainMenuScreenIfEnabled();
+        //    base.Btn_PlayGame();
+        //}
+
+        //private IEnumerable<UnityEngine.Object> BindMainMenuScreen()
+        //{
+        //    var root = m_MainMenuScreen.visualTree;
+            
+        //    var startButton = root.Q<Button>("start-button");
+        //    if (startButton != null)
+        //    {
+        //        startButton.clickable.clicked += Btn_PlayGame;
+        //    }
+
+        //    var exitButton = root.Q<Button>("exit-button");
+        //    if (exitButton != null)
+        //    {
+        //        exitButton.clickable.clicked += Btn_QuitGame;
+        //    }
+
+        //    return null;
+        //}
+
+        //void DisableMainMenuScreenIfEnabled()
+        //{
+        //    if(m_MainMenuScreen != null)
+        //    {
+        //        m_MainMenuScreen.postUxmlReload -= BindMainMenuScreen;
+        //        RTSUIEHelper.DisablePanelScreenIfEnabled(ref m_MainMenuScreen);
+        //    }
+        //}
+    }
+}
--- Assets/Tactical Prototyping/Scripts/UI/RTSUIEHelper.cs
+++ Assets/Tactical Prototyping/Scripts/UI/RTSUIEHelper.cs
@@ -1,95 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
-using Unity.UIElements.Runtime;
+//using UnityEngine.UIElements;
+//using Unity.UIElements.Runtime;
 
 namespace RTSPrototype
 {
     public class RTSUIEHelper
     {
-        #region Getters
-        public static bool IsPanelScreenEnabled(ref PanelRenderer screen)
-        {
-            UIElementsEventSystem _myEventSystem = null;
-            return IsPanelScreenEnabled(ref screen, out _myEventSystem);
-        }
+        //#region Getters
+        //public static bool IsPanelScreenEnabled(ref PanelRenderer screen)
+        //{
+        //    UIElementsEventSystem _myEventSystem = null;
+        //    return IsPanelScreenEnabled(ref screen, out _myEventSystem);
+        //}
 
-        public static bool IsPanelScreenEnabled(ref PanelRenderer screen, out UIElementsEventSystem myEventSystem)
-        {
-            myEventSystem = null;
-            return screen != null && screen.enabled &&
-                (myEventSystem = screen.gameObject.GetComponent<UIElementsEventSystem>()) != null &&
-                myEventSystem.enabled;
-        }
-        #endregion
+        //public static bool IsPanelScreenEnabled(ref PanelRenderer screen, out UIElementsEventSystem myEventSystem)
+        //{
+        //    myEventSystem = null;
+        //    return screen != null && screen.enabled &&
+        //        (myEventSystem = screen.gameObject.GetComponent<UIElementsEventSystem>()) != null &&
+        //        myEventSystem.enabled;
+        //}
+        //#endregion
 
-        #region TransitionHelpers
-        public static void SetScreenEnableState(ref PanelRenderer screen, bool state)
-        {
-            UIElementsEventSystem _myEventSystem = screen.gameObject.GetComponent<UIElementsEventSystem>();
-            SetScreenEnableState(ref screen, ref _myEventSystem, state);
-        }
+        //#region TransitionHelpers
+        //public static void SetScreenEnableState(ref PanelRenderer screen, bool state)
+        //{
+        //    UIElementsEventSystem _myEventSystem = screen.gameObject.GetComponent<UIElementsEventSystem>();
+        //    SetScreenEnableState(ref screen, ref _myEventSystem, state);
+        //}
 
-        public static void DisablePanelScreenIfEnabled(ref PanelRenderer screen)
-        {
-            UIElementsEventSystem _myEventSystem = null;
-            if (IsPanelScreenEnabled(ref screen, out _myEventSystem))
-            {
-                SetScreenEnableState(ref screen, ref _myEventSystem, false);
-            }
-        }
-        #endregion
+        //public static void DisablePanelScreenIfEnabled(ref PanelRenderer screen)
+        //{
+        //    UIElementsEventSystem _myEventSystem = null;
+        //    if (IsPanelScreenEnabled(ref screen, out _myEventSystem))
+        //    {
+        //        SetScreenEnableState(ref screen, ref _myEventSystem, false);
+        //    }
+        //}
+        //#endregion
 
-        #region TransitionLogic
-        //Screen Transition Logic
+        //#region TransitionLogic
+        ////Screen Transition Logic
 
-        public static void SetScreenEnableState(ref PanelRenderer screen, ref UIElementsEventSystem uieSystem, bool state)
-        {
-            if (state)
-            {
-                screen.visualTree.style.display = DisplayStyle.Flex;
-                screen.enabled = true;
-                uieSystem.enabled = true;
-            }
-            else
-            {
-                screen.visualTree.style.display = DisplayStyle.None;
-                screen.enabled = false;
-                uieSystem.enabled = false;
-            }
-        }
+        //public static void SetScreenEnableState(ref PanelRenderer screen, ref UIElementsEventSystem uieSystem, bool state)
+        //{
+        //    if (state)
+        //    {
+        //        screen.visualTree.style.display = DisplayStyle.Flex;
+        //        screen.enabled = true;
+        //        uieSystem.enabled = true;
+        //    }
+        //    else
+        //    {
+        //        screen.visualTree.style.display = DisplayStyle.None;
+        //        screen.enabled = false;
+        //        uieSystem.enabled = false;
+        //    }
+        //}
 
-        public static IEnumerator TransitionScreens(PanelRenderer from, PanelRenderer to)
-        {
-            from.visualTree.style.display = DisplayStyle.None;
-            from.gameObject.GetComponent<UIElementsEventSystem>().enabled = false;
+        //public static IEnumerator TransitionScreens(PanelRenderer from, PanelRenderer to)
+        //{
+        //    from.visualTree.style.display = DisplayStyle.None;
+        //    from.gameObject.GetComponent<UIElementsEventSystem>().enabled = false;
 
-            to.enabled = true;
+        //    to.enabled = true;
 
-            yield return null;
-            yield return null;
-            yield return null;
+        //    yield return null;
+        //    yield return null;
+        //    yield return null;
 
-            to.visualTree.style.display = DisplayStyle.Flex;
-            to.visualTree.style.visibility = Visibility.Hidden;
-            to.gameObject.GetComponent<UIElementsEventSystem>().enabled = true;
+        //    to.visualTree.style.display = DisplayStyle.Flex;
+        //    to.visualTree.style.visibility = Visibility.Hidden;
+        //    to.gameObject.GetComponent<UIElementsEventSystem>().enabled = true;
 
-            yield return null;
-            yield return null;
-            yield return null;
+        //    yield return null;
+        //    yield return null;
+        //    yield return null;
 
-            to.visualTree.style.visibility = Visibility.Visible;
+        //    to.visualTree.style.visibility = Visibility.Visible;
 
-            yield return null;
-            yield return null;
-            yield return null;
-            yield return null;
-            yield return null;
+        //    yield return null;
+        //    yield return null;
+        //    yield return null;
+        //    yield return null;
+        //    yield return null;
 
-            from.enabled = false;
-        }
-        #endregion
+        //    from.enabled = false;
+        //}
+        //#endregion
 
     }
 }
